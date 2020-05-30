0dfc8fbe0fca045639201278cb26a0dab960b155
Kee Tee <tkeeching@gmail.com>
2020-05-30 12:22:11 +1000

    Create StartBtn component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GameBoard from './components/GameBoard/GameBoard';
+import StartBtn from './components/StartBtn/StartBtn';
 
 function App() {
   return (
     <div className="App">
       <GameBoard />
+      <StartBtn />
     </div>
   );
 }
---
+++ src/components/StartBtn/StartBtn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StartBtn.css';
+
+function StartBtn() {
+
+  return (
+    <div className="startbtn">
+      <button>Start</button>
+    </div>
+  )
+}
+
+export default StartBtn;

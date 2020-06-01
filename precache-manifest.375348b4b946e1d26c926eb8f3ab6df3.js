a39b95d2aea2baed0399f25cd168a00627ded95a
Kee Tee <tkeeching@gmail.com>
2020-06-01 18:52:57 +1000

    Updates

---
+++ precache-manifest.375348b4b946e1d26c926eb8f3ab6df3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4741fc2f10e82619513cb989b6a37017",
+    "url": "/tic-tac-toe-react/%E2%80%A9/index.html"
+  },
+  {
+    "revision": "ff7a30dbb645e6cabc99",
+    "url": "/tic-tac-toe-react/%E2%80%A9/static/css/main.f529e0c2.chunk.css"
+  },
+  {
+    "revision": "a71a454a3b3552425a8d",
+    "url": "/tic-tac-toe-react/%E2%80%A9/static/js/2.f0733e3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-react/%E2%80%A9/static/js/2.f0733e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7a30dbb645e6cabc99",
+    "url": "/tic-tac-toe-react/%E2%80%A9/static/js/main.62fc5c03.chunk.js"
+  },
+  {
+    "revision": "e048f9741f10794a827d",
+    "url": "/tic-tac-toe-react/%E2%80%A9/static/js/runtime-main.ca6a0741.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe-react/%E2%80%A9/static/media/logo.5d5d9eef.svg"
+  }
+]);

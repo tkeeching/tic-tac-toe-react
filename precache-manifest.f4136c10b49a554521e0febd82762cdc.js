2d4f4ad3e9cdace13051b20dda4a4f6f7e26a225
Kee Tee <tkeeching@gmail.com>
2020-06-01 18:50:00 +1000

    Updates

---
+++ precache-manifest.f4136c10b49a554521e0febd82762cdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97c312875fc29bf79645a3c62ca66e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2ab5c1562c9c937e879",
+    "url": "/static/css/main.f529e0c2.chunk.css"
+  },
+  {
+    "revision": "a71a454a3b3552425a8d",
+    "url": "/static/js/2.f0733e3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f0733e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ab5c1562c9c937e879",
+    "url": "/static/js/main.87707bd6.chunk.js"
+  },
+  {
+    "revision": "3c648a670f047c2f708d",
+    "url": "/static/js/runtime-main.dfd47663.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

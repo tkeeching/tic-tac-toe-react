f8fddb4a3ef1265ac3df5ea26c33a625e7886e51
Kee Tee <tkeeching@gmail.com>
2020-06-05 17:54:19 +1000

    Update winning message

--- src/components/WinnerModal/WinnerModal.js
+++ src/components/WinnerModal/WinnerModal.js
@@ -4,9 +4,7 @@ import './WinnerModal.css';
 const WinnerModal = ({ winner, onClick }) => (
   <div className="winner-modal" onClick={onClick}>
     <div className="winner-modal__box">
-      Winner winner chicken dinner
-      <br />
-      {winner ? `Winner is ${winner}`: "It's a draw!"}
+      {winner ? `Player ${winner} wins`: "It's a draw!"}
     </div>
   </div>
 )
